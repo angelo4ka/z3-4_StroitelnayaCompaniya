9bf59b538d178dd3e0698c5195f72333082b8bc9
angelo4ka <karina3125@yandex.ru>
2022-12-08 16:53:57 +0300

    Добавлены поиск и сортировка

--- StroitelnayaCompaniyaApp/Stranici/PageSklad.xaml.cs
+++ StroitelnayaCompaniyaApp/Stranici/PageSklad.xaml.cs
@@ -45,6 +45,7 @@ namespace StroitelnayaCompaniyaApp.Stranici
             var Skladi = PodclucheniyeOdb.podcluchObj.Sklad.ToList();
 
             Skladi = PoiskSkladov(Skladi);
+            Skladi = SortirovkaSkladov(Skladi);
 
             DtgdSkladi.ItemsSource = ZapolnitVidMaterialov(Skladi);
         }
@@ -56,6 +57,64 @@ namespace StroitelnayaCompaniyaApp.Stranici
         /// <returns>Склады соответствующие поиску</returns>
         private List<Sklad> PoiskSkladov(List<Sklad> Skladi)
         {
+            string ParametrPoiska = TbxPoisk.Text;
+            if (ParametrPoiska != (string)TbxPoisk.Tag)
+            {
+                Skladi = Skladi.Where(x => x.Adres.Contains(ParametrPoiska)).ToList();
+            }
+
+            return Skladi;
+        }
+
+        /// <summary>
+        /// Сортировка складов по адресу
+        /// </summary>
+        /// <param name="Skladi">Список cкладов</param>
+        /// <returns>Отсортированный список складов</returns>
+        private List<Sklad> SortirovkaSkladov(List<Sklad> Skladi)
+        {
+            ComboBoxItem CmbxZnacheniyeSortirovki = (ComboBoxItem)CmbxSortirovka.SelectedItem;
+            if (CmbxZnacheniyeSortirovki.Content.ToString() != "Без сортировки")
+            {
+                if (TipSortirovki)
+                {
+                    switch ((string)CmbxZnacheniyeSortirovki.Tag)
+                    {
+                        case "ID":
+                            return Skladi = Skladi.OrderBy(x => x.ID).ToList();
+
+                        case "Adres":
+                            return Skladi = Skladi.OrderBy(x => x.Adres).ToList();
+
+                        case "Rasstoyanie":
+                            return Skladi = Skladi.OrderBy(x => x.Rasstoyanie).ToList();
+
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    switch ((string)CmbxZnacheniyeSortirovki.Tag)
+                    {
+                        case "ID":
+                            return Skladi = Skladi.OrderByDescending(x => x.ID).ToList();
+
+                        case "Adres":
+                            return Skladi = Skladi.OrderByDescending(x => x.Adres).ToList();
+
+                        case "Rasstoyanie":
+                            return Skladi = Skladi.OrderByDescending(x => x.Rasstoyanie).ToList();
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+
+
+
             string ParametrPoiska = TbxPoisk.Text;
             if (ParametrPoiska != (string)TbxPoisk.Tag)
             {
@@ -145,6 +204,11 @@ namespace StroitelnayaCompaniyaApp.Stranici
                 BtnVidSortirovki.ToolTip = "Сортировать по убыванию";
                 TipSortirovki = true;
             }
+        }
+
+        private void TbxPoisk_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbxPoisk.Text = "";
         }
     }
 }
